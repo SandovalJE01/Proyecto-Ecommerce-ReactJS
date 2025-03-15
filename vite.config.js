d9f4453f2127be253db72faa3165ac6c1c521939
Ezequiel <sandoval.jezequiel.01@gmail.com>
2025-03-15 13:32:04 -0300

    modificacion de rutas gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sandovalje01.github.io/Proyecto-Ecommerce-ReactJS/"
+  base: "/Proyecto-Ecommerce-ReactJS"
 })
